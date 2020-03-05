409766dba3f2908564ac7884ea26fa6c560477ec
Rammel Razon <rammel1012@gmail.com>
2020-03-05 13:39:06 -0800

    AABB collision

--- Demo/D3D12Project/App.cpp
+++ Demo/D3D12Project/App.cpp
@@ -33,6 +33,10 @@ struct RenderItem
 	// NumFramesDirty = gNumFrameResources so that each frame resource gets the update.
 	int NumFramesDirty = gNumFrameResources;
 
+	// adding 
+	XMVECTOR boundingboxminvertex;
+	XMVECTOR boundingboxmaxvertex;
+
 	// Index into GPU constant buffer corresponding to the ObjectCB for this render item.
 	UINT ObjCBIndex = -1;
 
@@ -71,6 +75,10 @@ private:
 	void UpdateObjectCBs(const GameTimer& gt);
 	void UpdateMainPassCB(const GameTimer& gt);
 
+	bool collisionCheck(XMVECTOR& firstboxmin, XMVECTOR& firstboxmax, XMMATRIX& firstboxworld, XMVECTOR& secondboxmin, XMVECTOR& secondboxmax, XMMATRIX& secondboxworld);
+	void calcAABB(std::vector<XMFLOAT3> boxVerts, XMFLOAT4X4& worldspace, XMVECTOR& boxmin, XMVECTOR& boxmax);
+	void CreateBoundingVolumes(std::vector<GeometryGenerator::Vertex>& vertPosArray,std::vector<XMFLOAT3>& boundingBoxVerts, std::vector<DWORD>& boundingBoxIndex);
+
     void BuildDescriptorHeaps();
     void BuildConstantBufferViews();
     void BuildRootSignature();
@@ -102,6 +110,13 @@ private:
 	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
     RenderItem* mBoxItemMovable;
     XMFLOAT3 keyboardInput = { 0.0f, 0.0f, 0.0f };
+
+	//global variables for the bounding box
+	RenderItem* firstbox = nullptr;
+	RenderItem* secondbox = nullptr;
+	std::vector<XMFLOAT3> boxBoundingVertPosArray;
+	std::vector<DWORD> boxBoundingVertIndexArray;
+	GeometryGenerator::MeshData box;
 
 	// Render items divided by PSO.
 	std::vector<RenderItem*> mOpaqueRitems;
@@ -168,7 +183,11 @@ bool App::Initialize()
     BuildRootSignature();
     BuildShadersAndInputLayout();
     BuildShapeGeometry();
-    BuildRenderItems();
+
+	//for creating the necessary vertices for bounding boxes
+	CreateBoundingVolumes(box.Vertices, boxBoundingVertPosArray, boxBoundingVertIndexArray);
+
+	BuildRenderItems();
     BuildFrameResources();
     BuildDescriptorHeaps();
     BuildConstantBufferViews();
@@ -198,6 +217,14 @@ void App::Update(const GameTimer& gt)
 {
     OnKeyboardInput(gt);
 	UpdateCamera(gt);
+
+	if (collisionCheck(firstbox->boundingboxminvertex, firstbox->boundingboxmaxvertex, XMLoadFloat4x4(&firstbox->World),
+		secondbox->boundingboxminvertex, secondbox->boundingboxmaxvertex, XMLoadFloat4x4(&secondbox->World))) {
+		OutputDebugString(L"Collision\n");
+	}
+	else {
+		OutputDebugString(L"No collision \n");
+	}
 
     // Cycle through the circular frame resource array.
     mCurrFrameResourceIndex = (mCurrFrameResourceIndex + 1) % gNumFrameResources;
@@ -330,6 +357,132 @@ void App::OnMouseMove(WPARAM btnState, int x, int y)
     mLastMousePos.x = x;
     mLastMousePos.y = y;
 }
+
+bool App::collisionCheck(XMVECTOR& firstboxmin, XMVECTOR& firstboxmax, XMMATRIX& firstboxworld, XMVECTOR& secondboxmin, XMVECTOR& secondboxmax, XMMATRIX& secondboxworld)
+{
+	//Is obj1's max X greater than obj2's min X? If not, obj1 is to the LEFT of obj2
+	if (XMVectorGetX(firstboxmax) > XMVectorGetX(secondboxmin)) {
+		//Is obj1's min X less than obj2's max X? If not, obj1 is to the RIGHT of obj2
+		if (XMVectorGetX(firstboxmin) < XMVectorGetX(secondboxmax)) {
+			//Is obj1's max Y greater than obj2's min Y? If not, obj1 is UNDER obj2
+			if (XMVectorGetY(firstboxmax) > XMVectorGetY(secondboxmin)) {
+				//Is obj1's min Y less than obj2's max Y? If not, obj1 is ABOVE obj2
+				if (XMVectorGetY(firstboxmin) < XMVectorGetY(secondboxmax)) {
+					//Is obj1's max Z greater than obj2's min Z? If not, obj1 is IN FRONT OF obj2
+					if (XMVectorGetZ(firstboxmax) > XMVectorGetZ(secondboxmin)) {
+						//Is obj1's min Z less than obj2's max Z? If not, obj1 is BEHIND obj2
+						if (XMVectorGetZ(firstboxmin) < XMVectorGetZ(secondboxmax)) {
+							//If we've made it this far, then the two bounding boxes are colliding
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+	//If the two bounding boxes are not colliding, then return false
+	//OutputDebugString(L"No collision \n");
+	return false;
+}
+
+void App::calcAABB(std::vector<XMFLOAT3> boxVerts, XMFLOAT4X4& worldspace, XMVECTOR& boxmin, XMVECTOR& boxmax)
+{
+	//OutputDebugString(L"Calculating AABB");
+	XMFLOAT3 minVertex = XMFLOAT3(FLT_MAX, FLT_MAX, FLT_MAX);
+	XMFLOAT3 maxVertex = XMFLOAT3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+	XMMATRIX world = XMLoadFloat4x4(&worldspace);
+	//Loop through the 8 vertices describing the bounding box
+	for (UINT i = 0; i < 8; i++)
+	{
+		//Transform the bounding boxes vertices to the objects world space
+		XMVECTOR Vert = XMVectorSet(boxVerts[i].x, boxVerts[i].y, boxVerts[i].z, 0.0f);
+		Vert = XMVector3TransformCoord(Vert, world);
+
+		//Get the smallest vertex 
+		minVertex.x = min(minVertex.x, XMVectorGetX(Vert));    // Find smallest x value in model
+		minVertex.y = min(minVertex.y, XMVectorGetY(Vert));    // Find smallest y value in model
+		minVertex.z = min(minVertex.z, XMVectorGetZ(Vert));    // Find smallest z value in model
+
+		//Get the largest vertex 
+		maxVertex.x = max(maxVertex.x, XMVectorGetX(Vert));    // Find largest x value in model
+		maxVertex.y = max(maxVertex.y, XMVectorGetY(Vert));    // Find largest y value in model
+		maxVertex.z = max(maxVertex.z, XMVectorGetZ(Vert));    // Find largest z value in model
+	}
+
+	//Store Bounding Box's min and max vertices
+	boxmin = XMVectorSet(minVertex.x, minVertex.y, minVertex.z, 0.0f);
+	boxmax = XMVectorSet(maxVertex.x, maxVertex.y, maxVertex.z, 0.0f);
+}
+
+void App::CreateBoundingVolumes(std::vector<GeometryGenerator::Vertex>& vertPosArray, std::vector<XMFLOAT3>& boundingBoxVerts, std::vector<DWORD>& boundingBoxIndex)
+{
+	XMFLOAT3 minVertex = XMFLOAT3(FLT_MAX, FLT_MAX, FLT_MAX);
+	XMFLOAT3 maxVertex = XMFLOAT3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+
+	for (UINT i = 0; i < vertPosArray.size(); i++)
+	{
+		// The minVertex and maxVertex will most likely not be actual vertices in the model, but vertices
+		// that use the smallest and largest x, y, and z values from the model to be sure ALL vertices are
+		// covered by the bounding volume
+
+		//Get the smallest vertex 
+		minVertex.x = min(minVertex.x, vertPosArray[i].Position.x);    // Find smallest x value in model
+		minVertex.y = min(minVertex.y, vertPosArray[i].Position.y);    // Find smallest y value in model
+		minVertex.z = min(minVertex.z, vertPosArray[i].Position.z);    // Find smallest z value in model
+
+		//Get the largest vertex 
+		maxVertex.x = max(maxVertex.x, vertPosArray[i].Position.x);    // Find largest x value in model
+		maxVertex.y = max(maxVertex.y, vertPosArray[i].Position.y);    // Find largest y value in model
+		maxVertex.z = max(maxVertex.z, vertPosArray[i].Position.z);    // Find largest z value in model
+	}
+
+	// Compute distance between maxVertex and minVertex
+	float distX = (maxVertex.x - minVertex.x) / 2.0f;
+	float distY = (maxVertex.y - minVertex.y) / 2.0f;
+	float distZ = (maxVertex.z - minVertex.z) / 2.0f;
+
+	// Create bounding box    
+	// Front Vertices
+	boundingBoxVerts.push_back(XMFLOAT3(minVertex.x, minVertex.y, minVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(minVertex.x, maxVertex.y, minVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(maxVertex.x, maxVertex.y, minVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(maxVertex.x, minVertex.y, minVertex.z));
+
+	// Back Vertices
+	boundingBoxVerts.push_back(XMFLOAT3(minVertex.x, minVertex.y, maxVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(maxVertex.x, minVertex.y, maxVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(maxVertex.x, maxVertex.y, maxVertex.z));
+	boundingBoxVerts.push_back(XMFLOAT3(minVertex.x, maxVertex.y, maxVertex.z));
+
+	DWORD* i = new DWORD[36];
+
+	// Front Face
+	i[0] = 0; i[1] = 1; i[2] = 2;
+	i[3] = 0; i[4] = 2; i[5] = 3;
+
+	// Back Face
+	i[6] = 4; i[7] = 5; i[8] = 6;
+	i[9] = 4; i[10] = 6; i[11] = 7;
+
+	// Top Face
+	i[12] = 1; i[13] = 7; i[14] = 6;
+	i[15] = 1; i[16] = 6; i[17] = 2;
+
+	// Bottom Face
+	i[18] = 0; i[19] = 4; i[20] = 5;
+	i[21] = 0; i[22] = 5; i[23] = 3;
+
+	// Left Face
+	i[24] = 4; i[25] = 7; i[26] = 1;
+	i[27] = 4; i[28] = 1; i[29] = 0;
+
+	// Right Face
+	i[30] = 3; i[31] = 2; i[32] = 6;
+	i[33] = 3; i[34] = 6; i[35] = 5;
+
+	for (int j = 0; j < 36; j++)
+		boundingBoxIndex.push_back(i[j]);
+}
  
 void App::OnKeyboardInput(const GameTimer& gt)
 {
@@ -355,8 +508,17 @@ void App::OnKeyboardInput(const GameTimer& gt)
     if (GetAsyncKeyState('K') & 0x8000)
         keyboardInput.y -= boxSpeed;
 
-    mBoxItemMovable->NumFramesDirty++;
-
+	//box translation//
+	XMMATRIX boxRotate = XMMatrixRotationY(0.5f * MathHelper::Pi);
+	XMMATRIX boxScale = XMMatrixScaling(2.0f, 2.0f, 2.0f);
+	XMMATRIX boxOffset = XMMatrixTranslation(keyboardInput.x, keyboardInput.y, keyboardInput.z);
+	XMMATRIX boxWorld = boxRotate * boxScale * boxOffset;
+	firstbox->Geo;
+	XMStoreFloat4x4(&firstbox->World, boxWorld);
+	//calculate new bounding box of first box
+	calcAABB(boxBoundingVertPosArray, firstbox->World, firstbox->boundingboxminvertex, firstbox->boundingboxmaxvertex);
+	//formerly mboxritemmovable
+    firstbox->NumFramesDirty++;
 
 }
  
@@ -386,9 +548,7 @@ void App::UpdateObjectCBs(const GameTimer& gt)
 		if(e->NumFramesDirty > 0)
 		{
 			XMMATRIX world = XMLoadFloat4x4(&e->World);
-            if (e->ObjCBIndex == 0) {
-                world = world * XMMatrixTranslation(keyboardInput.x, keyboardInput.y, keyboardInput.z);
-            }
+            
 			ObjectConstants objConstants;
 			XMStoreFloat4x4(&objConstants.World, XMMatrixTranspose(world));
 			currObjectCB->CopyData(e->ObjCBIndex, objConstants);
@@ -551,7 +711,7 @@ void App::BuildShadersAndInputLayout()
 void App::BuildShapeGeometry()
 {
     GeometryGenerator geoGen;
-	GeometryGenerator::MeshData box = geoGen.CreateBox(1.5f, 0.5f, 1.5f, 3);
+	box = geoGen.CreateBox(1.5f, 0.5f, 1.5f, 3);
 	GeometryGenerator::MeshData box2 = geoGen.CreateBox(1.5f, 0.5f, 1.5f, 3);
 	GeometryGenerator::MeshData grid = geoGen.CreateGrid(20.0f, 30.0f, 60, 40);
     
@@ -712,8 +872,10 @@ void App::BuildRenderItems()
 	boxRitem->IndexCount = boxRitem->Geo->DrawArgs["box"].IndexCount;
 	boxRitem->StartIndexLocation = boxRitem->Geo->DrawArgs["box"].StartIndexLocation;
 	boxRitem->BaseVertexLocation = boxRitem->Geo->DrawArgs["box"].BaseVertexLocation;
-    mBoxItemMovable = boxRitem.get();
+    firstbox = boxRitem.get();
 	mAllRitems.push_back(std::move(boxRitem));
+
+	calcAABB(boxBoundingVertPosArray, firstbox->World, firstbox->boundingboxminvertex, firstbox->boundingboxmaxvertex);
 
 
     auto boxRitem2 = std::make_unique<RenderItem>();
@@ -724,7 +886,10 @@ void App::BuildRenderItems()
     boxRitem2->IndexCount = boxRitem2->Geo->DrawArgs["box2"].IndexCount;
     boxRitem2->StartIndexLocation = boxRitem2->Geo->DrawArgs["box2"].StartIndexLocation;
     boxRitem2->BaseVertexLocation = boxRitem2->Geo->DrawArgs["box2"].BaseVertexLocation;
+	secondbox = boxRitem2.get();
     mAllRitems.push_back(std::move(boxRitem2));
+
+	calcAABB(boxBoundingVertPosArray, secondbox->World, secondbox->boundingboxminvertex, secondbox->boundingboxmaxvertex);
 
     auto gridRitem = std::make_unique<RenderItem>();
     gridRitem->World = MathHelper::Identity4x4();
